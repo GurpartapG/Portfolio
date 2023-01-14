99fde1acea9b4a56e707d5f761180e29980f59cb
GurpartapG <gpsgill@my.yorku.ca>
2023-01-14 02:14:41 -0500

    added header title to links and changed header letter spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,16 @@ export default function Header() {
         <header className={styles.header}>
             <h1 className={styles.headerText}>Gurpartap</h1>
             <div className={styles.navItems}>
-                <a href='#aboutme' className={styles.anchor}>
+                <a href='#aboutme' className={styles.anchor} title='About Me'>
                     <i className={`fa-solid fa-user-astronaut ${styles.icons}`}></i>
                 </a>
-                <a href='#education' className={styles.anchor}>
+                <a href='#education' className={styles.anchor} title='Education'>
                     <i className={`fa-solid fa-medal ${styles.icons}`}></i>
                 </a>
-                <a href='#projects' className={styles.anchor}>
+                <a href='#projects' className={styles.anchor} title='Projects'>
                     <i className={`fa-solid fa-diagram-project ${styles.icons}`}></i>
                 </a>
-                <a href='#footer' className={styles.anchor}>
+                <a href='#footer' className={styles.anchor} title='Contact'>
                     <i className={`fa-solid fa-envelope ${styles.icons}`}></i>
                 </a>
             </div>
