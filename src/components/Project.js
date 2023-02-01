bcdd55c6a9e1f219243150e97dbea8805d5b917c
GurpartapG <gpsgill@my.yorku.ca>
2023-01-31 20:24:04 -0500

    added audio to speech project

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,6 +8,7 @@ import tictactoe from '../assets/tictactoe.png'
 import movie from '../assets/movie.png'
 import blackjack from '../assets/blackjack.png'
 import phonebook from '../assets/phonebook.png'
+import audio from '../assets/audio_speech.png'
 import ImageOverlay from './ImageOverlay'
 
 export default function Projects() {
@@ -16,6 +17,16 @@ export default function Projects() {
       <div className={styles.wrapper}>
         <Sectionintroduction >PROJECTS</Sectionintroduction>
         <div className={styles.sectionContent}>
+
+          <ImageOverlay
+            imgSrc={audio}
+            title={"Audio To Speech"}
+            github={"https://github.com/GurpartapG/Audio-To-Speech---AssemblyAI.git"}>
+            An attempt at making a demo project to convert an audio file to text using Assembly AI.
+            <br></br><br></br>
+            Python &nbsp;&nbsp;&nbsp; AssemblyAI
+          </ImageOverlay>
+
           <ImageOverlay
             imgSrc={smokeSensor}
             title={"Smoke Detector Alarm"}
@@ -24,6 +35,7 @@ export default function Projects() {
             <br></br><br></br>
             C++
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={portfolioPic}
             title={"Portfolio Site"}
@@ -32,6 +44,7 @@ export default function Projects() {
             <br></br><br></br>
             Javascript &nbsp;&nbsp;&nbsp;  CSS &nbsp;&nbsp;&nbsp;  HTML&nbsp;&nbsp;&nbsp; REACT
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={soccer}
             title={"Soccer Knockout"}
@@ -40,6 +53,7 @@ export default function Projects() {
             <br></br><br></br>
             C#
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={tictactoe}
             title={"Tic Tac Toe"}
@@ -48,6 +62,7 @@ export default function Projects() {
             <br></br><br></br>
             Python
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={blackjack}
             title={"Black Jack"}
@@ -56,6 +71,7 @@ export default function Projects() {
             <br></br><br></br>
             Python
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={movie}
             title={"Box-Office"}
@@ -64,6 +80,7 @@ export default function Projects() {
             <br></br><br></br>
             C# &nbsp;&nbsp;&nbsp;  ASP
           </ImageOverlay>
+
           <ImageOverlay
             imgSrc={phonebook}
             title={"Phone-Book"}
@@ -72,6 +89,7 @@ export default function Projects() {
             <br></br><br></br>
             C
           </ImageOverlay>
+
         </div>
       </div>
     </section>
